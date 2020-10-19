c1647e59859e02b3e10198b1c2a6e9e21f318dd2
viktorlasenko <kenuay007@gmail.com>
2020-10-19 18:34:05 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.println("Введите первое число");
+        a = sc.nextInt();//Считываем первое число
+
+        System.out.println("Введите второе число");
+        b = sc.nextInt();//Считываем второе число
+
+        c = a + b;
+
+        System.out.println("c = " + c);
+    }
+}
